c9eb496319525a109df68013f4de045450343f05
Ilya Laptev <ilyalpt@gmail.com>
2019-06-01 20:07:33 +0300

    Added connection string for PickAutoDb

--- PickAuto/Startup.cs
+++ PickAuto/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PickAuto.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace PickAuto
 {
@@ -33,6 +35,10 @@ namespace PickAuto
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=PickAutoDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<PickAutoContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
