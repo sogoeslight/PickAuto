b1ff5c95d0cf4fdace528a153ae56c89cdeb37d9
Ilya Laptev <ilyalpt@gmail.com>
2019-06-03 16:35:56 +0300

    VCs generated

---
+++ PickAuto/Controllers/CarModelsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PickAuto.Models;
+
+namespace PickAuto.Controllers
+{
+    public class CarModelsController : Controller
+    {
+        private readonly PickAutoContext _context;
+
+        public CarModelsController(PickAutoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CarModels
+        public async Task<IActionResult> Index()
+        {
+            var pickAutoContext = _context.CarModel.Include(c => c.Gearbox).Include(c => c.Manufacturer);
+            return View(await pickAutoContext.ToListAsync());
+        }
+
+        // GET: CarModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carModel = await _context.CarModel
+                .Include(c => c.Gearbox)
+                .Include(c => c.Manufacturer)
+                .FirstOrDefaultAsync(m => m.CarModelId == id);
+            if (carModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(carModel);
+        }
+
+        // GET: CarModels/Create
+        public IActionResult Create()
+        {
+            ViewData["GearboxId"] = new SelectList(_context.GearBox, "GearboxId", "Name");
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturer, "ManufacturerId", "Country");
+            return View();
+        }
+
+        // POST: CarModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CarModelId,ManufacturerId,Name,ProductionYear,NumberOfSeats,FuelType,GearboxId,EngineLiters,WheelDrive")] CarModel carModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GearboxId"] = new SelectList(_context.GearBox, "GearboxId", "Name", carModel.GearboxId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturer, "ManufacturerId", "Country", carModel.ManufacturerId);
+            return View(carModel);
+        }
+
+        // GET: CarModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carModel = await _context.CarModel.FindAsync(id);
+            if (carModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["GearboxId"] = new SelectList(_context.GearBox, "GearboxId", "Name", carModel.GearboxId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturer, "ManufacturerId", "Country", carModel.ManufacturerId);
+            return View(carModel);
+        }
+
+        // POST: CarModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CarModelId,ManufacturerId,Name,ProductionYear,NumberOfSeats,FuelType,GearboxId,EngineLiters,WheelDrive")] CarModel carModel)
+        {
+            if (id != carModel.CarModelId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarModelExists(carModel.CarModelId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GearboxId"] = new SelectList(_context.GearBox, "GearboxId", "Name", carModel.GearboxId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturer, "ManufacturerId", "Country", carModel.ManufacturerId);
+            return View(carModel);
+        }
+
+        // GET: CarModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carModel = await _context.CarModel
+                .Include(c => c.Gearbox)
+                .Include(c => c.Manufacturer)
+                .FirstOrDefaultAsync(m => m.CarModelId == id);
+            if (carModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(carModel);
+        }
+
+        // POST: CarModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carModel = await _context.CarModel.FindAsync(id);
+            _context.CarModel.Remove(carModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CarModelExists(int id)
+        {
+            return _context.CarModel.Any(e => e.CarModelId == id);
+        }
+    }
+}
---
+++ PickAuto/Controllers/WorkersController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PickAuto.Models;
+
+namespace PickAuto.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private readonly PickAutoContext _context;
+
+        public WorkersController(PickAutoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Workers
+        public async Task<IActionResult> Index()
+        {
+            var pickAutoContext = _context.Worker.Include(w => w.Store);
+            return View(await pickAutoContext.ToListAsync());
+        }
+
+        // GET: Workers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var worker = await _context.Worker
+                .Include(w => w.Store)
+                .FirstOrDefaultAsync(m => m.WorkerId == id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            return View(worker);
+        }
+
+        // GET: Workers/Create
+        public IActionResult Create()
+        {
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "AddressLine1");
+            return View();
+        }
+
+        // POST: Workers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WorkerId,FirstName,LastName,Phone,Email,Occupation,Salary,StoreId")] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(worker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "AddressLine1", worker.StoreId);
+            return View(worker);
+        }
+
+        // GET: Workers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var worker = await _context.Worker.FindAsync(id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "AddressLine1", worker.StoreId);
+            return View(worker);
+        }
+
+        // POST: Workers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WorkerId,FirstName,LastName,Phone,Email,Occupation,Salary,StoreId")] Worker worker)
+        {
+            if (id != worker.WorkerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(worker);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkerExists(worker.WorkerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "AddressLine1", worker.StoreId);
+            return View(worker);
+        }
+
+        // GET: Workers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var worker = await _context.Worker
+                .Include(w => w.Store)
+                .FirstOrDefaultAsync(m => m.WorkerId == id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            return View(worker);
+        }
+
+        // POST: Workers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var worker = await _context.Worker.FindAsync(id);
+            _context.Worker.Remove(worker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkerExists(int id)
+        {
+            return _context.Worker.Any(e => e.WorkerId == id);
+        }
+    }
+}
